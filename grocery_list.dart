6a5e5ca3aeaf3c0cbcd8c87030525e27e2ec7f5f
Abdullah Alater <129957550+3ter26@users.noreply.github.com>
2023-08-10 09:04:20 +0300

    Add files via upload

---
+++ grocery_list.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:udemy5/models/grocery_item.dart';
+import 'package:udemy5/widgets/new_item.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'No groceries choosen yet!!',
+          style: TextStyle(
+            fontSize: 25.5,
+          ),
+        ),
+      ),
+    );
+
+    return _groceryItems.isNotEmpty
+        ? Scaffold(
+            appBar: AppBar(
+              title: const Text('Your Groceries'),
+              actions: [
+                IconButton(
+                  onPressed: _addItem,
+                  icon: const Icon(Icons.add),
+                ),
+              ],
+            ),
+            body: ListView.builder(
+              itemCount: _groceryItems.length,
+              itemBuilder: (ctx, index) => Dismissible(
+                onDismissed: (direction) {
+                  _removeItem(_groceryItems[index]);
+                },
+                key: ValueKey(_groceryItems[index].id),
+                child: ListTile(
+                  title: Text(_groceryItems[index].name),
+                  leading: Container(
+                    width: 24,
+                    height: 24,
+                    color: _groceryItems[index].category.color,
+                  ),
+                  trailing: Text(_groceryItems[index].quantity.toString()),
+                ),
+              ),
+            ),
+          )
+        : content;
+  }
+}
